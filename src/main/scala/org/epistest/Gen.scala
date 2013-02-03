f475bc662bb8b541b9d63d65606424b05988e61b
Tony Morris <tmorris@tmorris.net>
2013-02-03 21:56:40 +1000

    rng

--- src/main/scala/org/epistest/Gen.scala
+++ src/main/scala/org/epistest/Gen.scala
@@ -1,6 +1,24 @@
 package org.epistest
 
+import scalaz._
+
 sealed trait Gen[~>[-_, +_], -A, +B] {
   val value: A ~> Rng[B]
 
+  def map[X](f: B => X)(implicit A: Arrow[~>]): Gen[~>, A, X] =
+    Gen(A.mapsnd(value)(_ map f))
+
+}
+
+object Gen {
+  type RGen[-A, +B] =
+  Gen[Function1, A, B]
+
+  type OptionGen[-A, +B] =
+  Gen[ToOption, A, B]
+
+  private[epistest] def apply[~>[-_, +_], A, B](v: A ~> Rng[B]): Gen[~>, A, B] =
+    new Gen[~>, A, B] {
+      val value = v
+    }
 }
