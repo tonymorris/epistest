2675b451b015100b5c1a342a48f9eb90fb1802f8
Tony Morris <tmorris@tmorris.net>
2013-02-14 12:36:33 +1100

    comonad

--- src/main/scala/org/epistest/GenResult.scala
+++ src/main/scala/org/epistest/GenResult.scala
@@ -1,5 +1,7 @@
 package org.epistest
 
+import scalaz._
+
 sealed trait GenResult[+A] {
   val size: Size
   val value: A
@@ -9,6 +11,10 @@ sealed trait GenResult[+A] {
 
   def coflatMap[B](f: GenResult[A] => B): GenResult[B] =
     GenResult(size, f(this))
+
+  def duplicate: GenResult[GenResult[A]] =
+    GenResult(size, GenResult(size, value))
+
 }
 
 object GenResult {
@@ -17,4 +23,17 @@ object GenResult {
       val size = s
       val value = v
     }
+
+  implicit val GenResultComonad: Comonad[GenResult] =
+    new Comonad[GenResult] {
+      def map[A, B](a: GenResult[A])(f: A => B) =
+        a map f
+      def cobind[A, B](a: GenResult[A])(f: GenResult[A] => B) =
+        a coflatMap f
+      def cojoin[A](x: GenResult[A]) =
+        x.duplicate
+      def copoint[A](x: GenResult[A]) =
+        x.value
+    }
+
 }
