1edf5940f701a2c00b70cd45c7619793353ea2fb
Tony Morris <tmorris@tmorris.net>
2013-02-21 20:00:54 +1100

    property

--- src/main/scala/org/epistest/Property.scala
+++ src/main/scala/org/epistest/Property.scala
@@ -2,7 +2,7 @@ package org.epistest
 
 import scalaz._, Scalaz._
 
-sealed trait Property[-A, +R] {
+sealed trait Property[A, R] {
   val run: A => R
 
   def contramap[B](f: B => A): Property[B, R] =
@@ -11,19 +11,19 @@ sealed trait Property[-A, +R] {
   def map[S](f: R => S): Property[A, S] =
     Property(f compose run)
 
-  def flatMap[AA <: A, S](f: R => Property[AA, S]): Property[AA, S] =
+  def flatMap[S](f: R => Property[A, S]): Property[A, S] =
     Property(a => f(run(a)) run a)
 
-  def ap[AA <: A, X](f: Property[AA, R => X]): Property[AA, X] =
+  def ap[X](f: Property[A, R => X]): Property[A, X] =
     for {
       ff <- f
       aa <- this
     } yield ff(aa)
 
-  def zip[AA <: A, X](q: Property[AA, X]): Property[AA, (R, X)] =
+  def zip[X](q: Property[A, X]): Property[A, (R, X)] =
     zipWith(q)(a => (a, _))
 
-  def zipWith[AA <: A, B, C](r: Property[AA, B])(f: R => B => C): Property[AA, C] =
+  def zipWith[B, C](r: Property[A, B])(f: R => B => C): Property[A, C] =
     r.ap(map(f))
 
   def compose[S](p: Property[S, A]): Property[S, R] =
@@ -43,16 +43,88 @@ sealed trait Property[-A, +R] {
       case \/-(b) => \/-(p.run(b))
     }
 
-  def |||[RR >: R, B](p: Property[B, RR]): Property[A \/ B, RR] =
+  def |||[B](p: Property[B, R]): Property[A \/ B, R] =
     Property {
       case -\/(a) => run(a)
       case \/-(b) => p.run(b)
     }
 
-  def check[AA <: A](tests: Int, sz: Size)(implicit D: Decision[R], G: Gen[AA], S: Shrink[AA]): R = {
-    error("")
+  def check(tests: Int, sz: Size)(implicit D: Decision[R], T: DataGenShrink[A]): Result[A] = {
+    val w =
+      T.gen fill tests map (l => {
+        @annotation.tailrec
+        def successloop(x: List[A], r: Result[A]): Result[A] =
+          x match {
+            case Nil =>
+              r
+            case h::t =>
+              if(D(run(h)))
+                successloop(t, r.withSucceed(h::_))
+              else {
+                @annotation.tailrec
+                def failedloop(y: EphemeralStream[A], v: (List[A], Option[A])): (List[A], Option[A]) =
+                  if(y.isEmpty)
+                    v
+                  else {
+                    val (z, o) = v
+                    val hh = y.head()
+                    if(D(run(hh)))
+                      failedloop(y.tail(), v)
+                    else if (o exists (_ == hh))
+                      (z, Some(hh))
+                    else
+                      failedloop(T.shrink(hh), (z, Some(hh)))
+
+                  }
+
+                r match {
+                  case Succeed(s) => {
+                    val (e, z) = failedloop(T.shrink(h), (Nil, None))
+                    Failed(s, e, OneOrTwo(h) optionTwo z)
+                  }
+                  case Failed(s, k, i) => Failed(s, k, i)
+                }
+              }
+          }
+
+        successloop(l, Succeed(Nil))
+      })
+
+    w run sz
   }
+
 }
+
+sealed trait DataGenShrink[A] {
+  val gen: Gen[A]
+  val shrink: Shrink[A]
+
+  def zip[B](b: DataGenShrink[B]): DataGenShrink[(A, B)] =
+    DataGenShrink(gen zip b.gen, shrink zip b.shrink)
+
+}
+
+object DataGenShrink {
+  def apply[A](g: Gen[A], s: Shrink[A]): DataGenShrink[A] =
+    new DataGenShrink[A] {
+      val gen = g
+      val shrink = s
+    }
+
+  implicit val IntDataGenShrink: DataGenShrink[Int] =
+    apply(Gen.int, Shrink.int)
+}
+
+sealed trait Result[+A] {
+  def withSucceed[AA >: A](f: List[A] => List[AA]): Result[AA] =
+    this match {
+      case Succeed(s) => Succeed(f(s))
+      case Failed(s, h, i) => Failed(s, h, i)
+    }
+}
+case class Succeed[+A](succeed: List[A]) extends Result[A]
+case class Failed[+A](succeed: List[A], shrinksucceed: List[A], failed: OneOrTwo[A]) extends Result[A]
+
 
 object Property {
   def apply[A, R](r: A => R): Property[A, R] =
