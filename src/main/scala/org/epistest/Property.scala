a6d6bd6320cc7c3e70d959766697d0e0ba1adb11
Tony Morris <tmorris@tmorris.net>
2013-02-21 20:22:04 +1100

    property

--- src/main/scala/org/epistest/Property.scala
+++ src/main/scala/org/epistest/Property.scala
@@ -143,94 +143,9 @@ object Property {
 }
 
 object Main {
-  val p1 = Property((n: Int) => if(n > 200 && n < 10000000) false else n + 0 == n)
+  val p1 = Property._2((x: Int, y: Int) => x + y == y + x)
   def main(args: Array[String]) {
     val r = p1.check(100, Size(5))
     println(r)
   }
 }
-
-/*
-sealed trait Result[+A]
-case class Exhausted[+A]() extends Result[A]
-case class Succeed[+A](succeed: List[A]) extends Result[A]
-case class Failed[+A](succeed: List[A], shrinksucceed: List[A], failed: OneOrTwo[A]) extends Result[A]
-
-
-// case class PropertyWithDiscarded[-A](run: A => (Interval, Boolean))
-case class Property[-A](run: A => Boolean) {
-  def check[AA <: A](g: Gen[AA], sh: Shrink[AA], ex: Interval[AA], tests: Int, sz: Size)(implicit E: Enum[AA]): Result[AA] = {
-    val o = Diev.empty[AA] + ((ex.min, ex.max))
-    val y = g fill tests map (l => {
-      def f(x: List[AA], d: Diev[AA], r: Result[AA]): Result[AA] =
-        x match {
-          case Nil => r
-          case h::t => {
-            if(run(h)) {
-              val e = d + h
-              if(e === o)
-                Exhausted()
-              else
-                f(t, e, r match {
-                  case Exhausted() => Exhausted()
-                  case Succeed(s) => Succeed(h::s)
-                  case Failed(s, i, f) => Failed(s, i, f)
-                })
-            } else {
-              def g(x: EphemeralStream[AA], v: (List[AA], Option[AA])): (List[AA], Option[AA]) =
-                if(x.isEmpty)
-                  v
-                else {
-                  val hh = x.head()
-                  v match {
-                    case (p, qq) => {
-                      val d = run(hh)
-                      if(d)
-                        g(x.tail(), (hh :: p, qq))
-                      else {
-                        if(qq exists (_ == hh))
-                          (p, Some(hh))
-                        else
-                          g(sh(hh), (p, Some(hh)))
-                      }
-                    }
-                  }
-                }
-
-              r match {
-                case Exhausted() => Exhausted()
-                case Succeed(s) => {
-                  val (e, z) = g(sh(h), (Nil, None))
-                  Failed(s, e, OneOrTwo(h) optionTwo z)
-                }
-                case Failed(s, i, f) => Failed(s, i, f)
-              }
-            }
-          }
-        }
-      f(l, Diev.empty, Succeed(Nil))
-    })
-
-    y.run(sz)
-  }
-
-  def check2[AA <: A](g: Gen[AA], sh: Shrink[AA], tests: Int, sz: Size): Result[AA] = {
-    error("")
-  }
-
-}
-
-object Property {
-  val plusZero = Property((n: Int) => n + 0 == n)
-  val plusZeroBreaks = Property((n: Int) => if(n > 24 && n < 100000000) n == 77 else n + 0 == n)
-
-}
-
-object Main {
-  def main(args: Array[String]) {
-    val p = Property.plusZeroBreaks
-    val r = p.check(Gen.int, Shrink.int, Interval.Range.int, 100, Size.nosize)
-    println(r)
-  }
-}
-*/
